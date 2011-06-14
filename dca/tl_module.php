6b788e78e2cfa16389f5d014104944962eac64d8
Andreas Schempp <andreas@schempp.ch>
2011-06-14 16:11:58 +0000

    Some more commits

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -36,7 +36,12 @@ foreach( $GLOBALS['TL_DCA']['tl_module']['palettes'] as $name => $palette )
 	if ($name == '__selector__')
 		continue;
 	
-	$GLOBALS['TL_DCA']['tl_module']['palettes'][$name] = str_replace('{expert_legend:hide}', '{expert_legend:hide},aliasPages', $palette);
+	$GLOBALS['TL_DCA']['tl_module']['palettes'][$name] = str_replace('{expert_legend:hide}', '{expert_legend:hide},aliasPages', $palette, $count);
+	
+	if (!$count)
+	{
+		$GLOBALS['TL_DCA']['tl_module']['palettes'][$name] .= ';{expert_legend:hide},aliasPages;';
+	}
 }
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['modulealias'] = '{title_legend},name,type;{config_legend},aliasModules';
